3cba7ac70adc31bef50068d7e164050574d0d946
HelmiDev <helmipaty@gmail.com>
2024-11-25 00:53:51 +0100

    random pic for DOB

--- components/apps/helmi.js
+++ components/apps/helmi.js
@@ -737,6 +737,7 @@ function Thoughts() {
         "https://res.cloudinary.com/dmvxysqvl/image/upload/v1731797379/1731796986937_nhmhh3.jpg",
         "https://res.cloudinary.com/dmvxysqvl/video/upload/dabdoub_klcaaw.mp4?_s=vp-2.1.0" , 
         "https://res.cloudinary.com/dmvxysqvl/image/upload/v1731946719/IMG_20241118_171742_gzoucs.jpg", 
+        "https://res-console.cloudinary.com/dmvxysqvl/thumbnails/v1/image/upload/v1732492285/SU1HXzIwMjMxMDI4XzA4MDAxN19jZHpqbng=/drilldown",
         "https://res.cloudinary.com/dmvxysqvl/image/upload/v1731946719/IMG_20241118_171758_xin0i1.jpg", 
         "https://res.cloudinary.com/dmvxysqvl/image/upload/v1731797379/1731796986946_na78sr.jpg",
         "https://res.cloudinary.com/dmvxysqvl/image/upload/v1680561729/media/post_images/6cacd6a4-7d9e-48d7-bca3-1577d0f0370d_qkeeeh.jpg",
