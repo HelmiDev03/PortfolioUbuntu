146a7c64cd42172b10afe3933d44573974066987
HelmiDev03 <helmipaty@gmail.com>
2025-09-11 17:42:13 +0100

    added

--- components/base/ubuntu_app.js
+++ components/base/ubuntu_app.js
@@ -6,7 +6,8 @@ export class UbuntuApp extends Component {
     super();
     this.state = {
       dragging: false,
-      dragStartTime: 0
+      dragStartTime: 0,
+      isSmallScreen: false
     };
     this.defaultPosition = { x: 0, y: 0 };
   }
@@ -21,6 +22,21 @@ export class UbuntuApp extends Component {
         console.error("Error parsing saved position:", e);
       }
     }
+    
+    // Check screen size on mount
+    this.checkScreenSize();
+    
+    // Add event listener for window resize
+    window.addEventListener('resize', this.checkScreenSize);
+  }
+  
+  componentWillUnmount() {
+    // Remove event listener when component unmounts
+    window.removeEventListener('resize', this.checkScreenSize);
+  }
+  
+  checkScreenSize = () => {
+    this.setState({ isSmallScreen: window.innerWidth < 1000});
   }
 
   openApp = () => {
@@ -49,6 +65,32 @@ export class UbuntuApp extends Component {
   };
 
   render() {
+    // Create the app content element
+    const appContent = (
+      <div
+        className={`p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-ub-orange focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white ${this.state.isSmallScreen ? 'cursor-pointer' : 'cursor-move'}`}
+        id={"app-" + this.props.id}
+        onDoubleClick={this.openApp}
+        tabIndex={0}
+      >
+        <img
+          width="40px"
+          height="40px"
+          className="mb-1 w-10"
+          src={this.props.icon}
+          alt={"Ubuntu " + this.props.name}
+          draggable="false"
+        />
+        {this.props.name}
+      </div>
+    );
+    
+    // If small screen, return the app content without Draggable wrapper
+    if (this.state.isSmallScreen) {
+      return appContent;
+    }
+    
+    // Otherwise, wrap with Draggable
     return (
       <Draggable
         defaultPosition={this.defaultPosition}
@@ -56,22 +98,7 @@ export class UbuntuApp extends Component {
         onStop={this.handleDragStop}
         bounds="parent"
       >
-        <div
-          className="p-1 m-px z-10 bg-white bg-opacity-0 hover:bg-opacity-20 focus:bg-ub-orange focus:bg-opacity-50 focus:border-yellow-700 focus:border-opacity-100 border border-transparent outline-none rounded select-none w-24 h-20 flex flex-col justify-start items-center text-center text-xs font-normal text-white cursor-move"
-          id={"app-" + this.props.id}
-          onDoubleClick={this.openApp}
-          tabIndex={0}
-        >
-          <img
-            width="40px"
-            height="40px"
-            className="mb-1 w-10"
-            src={this.props.icon}
-            alt={"Ubuntu " + this.props.name}
-            draggable="false"
-          />
-          {this.props.name}
-        </div>
+        {appContent}
       </Draggable>
     );
   }
